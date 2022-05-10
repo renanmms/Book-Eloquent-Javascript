a373405f14378f49ef4d7e35e6676b1f5f67d0b0
Renan Martins <renanmartins1999@hotmail.com>
2022-05-09 23:36:01 -0300

    Initializing repository

---
+++ chapter1.js
@@ -0,0 +1,33 @@
+// Order of precedence
+console.log("----- Arithmetic -----");
+console.log(100+4*11);
+console.log((100+4)*11);
+
+console.log(314 % 100);
+console.log(144 % (12 + 1));
+
+console.log("----- Numbers -----");
+console.log(typeof(2));
+let isInt = Number.isInteger(Number.MAX_VALUE)
+console.log(Infinity);
+
+console.log("----- Strings -----");
+console.log("Down on the sea ");
+console.log('Lie on the Ocean');
+console.log(`Float on the ocean`)
+
+console.log("----- Escaping characters -----");
+console.log("");
+
+// Newline escaping character
+console.log("\n");
+
+// Backslash escaping character
+console.log("\t");
+
+console.log("This is the first line\nAnd this is the second")
+
+// Printing the backslash instead of the special character
+console.log("\\o/");
+
+console.log("A newline character is written like \"\\n\".");
---
+++ intro.js
@@ -0,0 +1,9 @@
+// var total = 0;
+// var count = 1;
+
+// while( count <= 10 ){
+//     total += count;
+//     count++;
+// }
+
+// console.log(total)
