2df5de0905c4a52be4885b203c0632bfa04766c3
Renan Martins <renanmartins1999@hotmail.com>
2022-05-11 22:59:06 -0300

    Adding chapter 2

---
+++ chapter2.js
@@ -0,0 +1,14 @@
+// Binding
+let caught = 5*5;
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
