fa092ad883d6df5e301ae7b5e1b6ef8392647264
Renan Martins <renanmartins1999@hotmail.com>
2022-05-15 23:35:03 -0300

    Adding some loops

--- chapter2.js
+++ chapter2.js
@@ -1,5 +1,5 @@
 // Binding
-let caught = 5*5;
+let caught = 5 * 5;
 let ten = 10;
 console.log(ten * ten);
 
@@ -12,3 +12,23 @@ console.log(mood);
 let luigisDebt = 140;
 luigisDebt = luigisDebt - 35;
 console.log(luigisDebt);
+
+var n = "Renan";
+const greeting = "Hello ";
+console.log(`${greeting}${n}`)
+
+console.log(Math.max(7, 9));
+
+console.log("---- Loops ----");
+
+let even = 1;
+while (even < 1000) {
+    even += 2;
+    console.log(even);
+}
+
+
+do {  
+    console.log((even % 3) ? "~" : "^");
+    even--;
+}while(even >= 0)
